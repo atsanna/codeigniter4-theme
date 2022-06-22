9d3b976c0202a65080582eea8b58d1419ba06627
Antonio Sanna <atsanna@tiscali.it>
2022-06-22 16:47:06 +0200

    First Commit

--- src/Theme.php
+++ src/Theme.php
@@ -15,7 +15,7 @@ include_once __DIR__ . '/Config/Constants.php';
 
 class Theme
 {
-        // Return Theme Version
+    // Return Theme Version
     public function getVersion()
     {
         return THEME_VERSION;
